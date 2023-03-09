208433002364ffdd05baa35a2870157255f82c08
иван матвеев <ivat1056@gmail.com>
2023-03-09 16:41:32 +0300

    12

--- WPFMDK01Sesian1/MainWindow.xaml.cs
+++ WPFMDK01Sesian1/MainWindow.xaml.cs
@@ -35,6 +35,7 @@ namespace WPFMDK01Sesian1
             Number.Text = "";
             Password.Text = "";
             Key.Text = "";
+
         }
 
         
@@ -44,8 +45,7 @@ namespace WPFMDK01Sesian1
         }
        
         private void PasswordBox_KeyDown(object sender, KeyEventArgs e)
-        {
-
+        { 
             if (e.Key == System.Windows.Input.Key.Enter)
             {
                 Emploe em = ClassBase.ep.Emploe.FirstOrDefault(z => z.Number == Number.Text && z.Password == Password.Text);
@@ -55,45 +55,100 @@ namespace WPFMDK01Sesian1
                 }
                 else
                 {
-                    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&!*№";
-                    var stringChars = new char[8];
-                    var random = new Random();
-
-                    for (int i = 0; i < stringChars.Length; i++)
+                    if(Key.Text == "")
                     {
-                        stringChars[i] = chars[random.Next(chars.Length)];
+                        Auto();
                     }
+                 
 
-                    var finalString = new String(stringChars);
+                     
+                }
+            }
+        }
 
-                    MessageBox.Show($"Введите код {finalString}  в течение 10 секунд" ) ;
+        public void Auto()
+        {
+            string finalString = GenRandom();
+            MessageBox.Show($"Введите код {finalString}  в течение 10 секунд");
 
-                    KeyV.Visibility = Visibility.Visible ;
+            Key.Visibility = Visibility.Visible;
+            KeyN.Visibility = Visibility.Visible;
 
-                    _time = TimeSpan.FromSeconds(10);
+            _time = TimeSpan.FromSeconds(20);  // Таймер
 
-                    _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
-                    {
-                        tbTime.Text = _time.ToString("c");
-                        if (_time == TimeSpan.Zero)
-                        {
-                            _timer.Stop();
-                            KeyV.Visibility = Visibility.Hidden;
-                            MessageBox.Show("Обновите код");
-                        }
-                        else
-                        {
-                            _time = _time.Add(TimeSpan.FromSeconds(-1));
-                        }
-                    }, Application.Current.Dispatcher);
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 2), DispatcherPriority.Normal, delegate
+            {
+                tbTime.Text = _time.ToString("c");
+                if (_time == TimeSpan.Zero)
+                {
+                    _timer.Stop();
+                    MessageBox.Show("Обновите код");
+                    Key.Visibility = Visibility.Hidden;
+                    Refac.Visibility = Visibility.Visible;
 
-                    _timer.Start();
-                   
+                }
+                else
+                {
+                    _time = _time.Add(TimeSpan.FromSeconds(-1));
+                  
+                }
+
+                
+            }, Application.Current.Dispatcher);
+
+            Emploe em1 = ClassBase.ep.Emploe.FirstOrDefault(z => z.Number == Number.Text && z.Password == Password.Text);
+            if (em1 == null)
+            {
+                MessageBox.Show("Такого сотрудника нет");
+            }
+            else
+            {
+                if (Key.Text == finalString)
+                {
+                    Emploe emploe = ClassBase.ep.Emploe.FirstOrDefault(x => x.Number == Number.Text);
+                    MessageBox.Show($"Ваша роль {emploe.Rols.Name}");
+                }
+                else
+                {
+                    MessageBox.Show("Код введен не верно");
 
                 }
             }
-
+            _timer.Start();
         }
+        public string GenRandom()
+        {
+            var chars1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var chars2 = "abcdefghijklmnopqrstuvwxyz"; // Генерация рандомного числа  
+            var chars3 = "0123456789";
+            var chars4 = "@#$%&!*№";
+
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i <= 1; i++)
+            {
+                stringChars[i] = chars1[random.Next(chars1.Length)];
+            }
+
+            for (int i = 2; i <= 3; i++)
+            {
+                stringChars[i] = chars2[random.Next(chars2.Length)];
+            }
+            for (int i = 4; i <= 5; i++)
+            {
+                stringChars[i] = chars3[random.Next(chars3.Length)];
+            }
+            for (int i = 6; i <= 7; i++)
+            {
+                stringChars[i] = chars4[random.Next(chars4.Length)];
+            }
+
+
+            var finalString = new String(stringChars);
+            return finalString;
+        }
+        
 
         private void btn_In_Click(object sender, RoutedEventArgs e)
         {
@@ -102,7 +157,7 @@ namespace WPFMDK01Sesian1
 
         private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+           
         }
     }
 }
